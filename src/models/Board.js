7d8130229c2f8bef6d424a0fb8de45981e48582d
Levi McGlone <mcglonelevi@gmail.com>
2021-03-26 16:15:12 -0400

    write implementation for board#generateBoardArray

--- src/models/Board.js
+++ src/models/Board.js
@@ -1,4 +1,9 @@
 export default class Board {
+    static PLAYERS = {
+        FIRST_PLAYER: "X",
+        SECOND_PLAYER: "Y",
+    };
+
     static validateMoves(moves) {
         moves.forEach(([x, y]) => {
             if (x > 2 || y > 2) {
@@ -11,5 +16,23 @@ export default class Board {
         Board.validateMoves(moves);
 
         this.moves = moves;
+    }
+
+    /**
+     * Given a move set, we can determine the layout of the tic tac toe board.  By convention, x
+     * will be first player, o will be second player. The result will be a 3x3 of "x"|"o"|null
+     */
+    generateBoardArray() {
+        const baseBoard = [
+            [null, null, null],
+            [null, null, null],
+            [null, null, null]
+        ];
+
+        return this.moves.reduce((board, [x, y], currentIndex) => {
+            const player = currentIndex % 2 === 0 ? Board.PLAYERS.FIRST_PLAYER : Board.PLAYERS.SECOND_PLAYER;
+            board[x][y] = player;
+            return board;
+        }, baseBoard);
     }
 }
