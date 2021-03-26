e5bdfe177f3b6c09a9925c329aa47c1874c5f809
Levi McGlone <mcglonelevi@gmail.com>
2021-03-26 18:06:52 -0400

    add useTicTacToe logic

--- src/hooks/useTicTacToe.js
+++ src/hooks/useTicTacToe.js
@@ -3,5 +3,32 @@ import { useState } from 'react';
 import Board from '../models/Board';
 
 export default function useTicTacToe() {
-    return null;
+    const [moves, setMoves] = useState([]);
+
+    const move = (newMove) => {
+        setMoves((currMoves) => {
+            return [...currMoves, newMove];
+        });
+    };
+
+    const reset = () => {
+        setMoves([]);
+    };
+
+    const back = () => {
+        setMoves((currMoves) => {
+            return currMoves.slice(0, currMoves.length - 1);
+        });  
+    };
+
+    const ticTacToeBoard = new Board(moves);
+
+    return {
+        board: ticTacToeBoard.generateBoardArray(),
+        move,
+        reset,
+        back,
+        winnerOrTie: ticTacToeBoard.getWinnerOrTie(),
+        nextTurn: Board.getPlayerForTurn(moves.length + 1),
+    };
 }
