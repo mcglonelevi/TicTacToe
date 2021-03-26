cd3d738edf45beb7c030c3775f2428547c5fdd19
Levi McGlone <mcglonelevi@gmail.com>
2021-03-26 15:58:57 -0400

    write basic skeleton board class and test case stubs

---
+++ src/models/Board.test.js
@@ -0,0 +1,34 @@
+import Board from './Board';
+
+describe('Board', () => {
+    xdescribe('constructor', () => {
+        it('stores moves in class variable', () => {
+            
+        });
+    });
+
+    xdescribe('#getWinnerOrTie', () => {
+        it("returns \"x\" in x win state", () => {
+
+        });
+
+        it("returns \"o\" in o win state", () => {
+
+        });
+
+        it("returns \"tie\" in tie state", () => {
+
+        });
+
+        it("returns null if no tie or winner", () => {
+
+        });
+    });
+   
+    
+    xdescribe('#generateBoardArray', () => {
+        it("returns correct multi-dimensional array given an array of coordinates", () => {
+
+        });
+    });
+});
