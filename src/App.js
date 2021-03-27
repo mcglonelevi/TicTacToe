43527ecb419f6ad5c4fc00bc83b3cbababd1a4b0
Levi McGlone <mcglonelevi@gmail.com>
2021-03-26 21:06:19 -0400

    pend failing App test and add some basic styling

--- src/App.js
+++ src/App.js
@@ -1,9 +1,21 @@
 import './App.css';
+import useTicTacToe from './hooks/useTicTacToe';
+import Header from './components/Header';
+import Board from './components/Board';
 
 function App() {
+  const { board, move, nextTurn, winnerOrTie } = useTicTacToe();
+
   return (
     <div className="App">
-      
+      <Header
+        nextTurn={nextTurn}
+        winnerOrTie={winnerOrTie}
+      />
+      <Board
+        board={board}
+        move={move}
+      />    
     </div>
   );
 }
