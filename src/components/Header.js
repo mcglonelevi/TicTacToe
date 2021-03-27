b4f51edd0c8938df6fc7174f7bacc557d2d70133
Levi McGlone <mcglonelevi@gmail.com>
2021-03-26 20:46:13 -0400

    add header implementation

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,4 +1,14 @@
-export default function Header({ nextTurn, winnerOrTie }) {
+import BoardModel from '../models/Board';
 
+export default function Header({ nextTurn, winnerOrTie }) {
+    if (winnerOrTie === null) {
+        return <h1>It is {nextTurn}'s turn.</h1>;
+    }
+  
+    if (winnerOrTie === BoardModel.TIE_STATE) {
+        return <h1>It was a tie.</h1>;
+    }
+  
+    return <h1>{winnerOrTie} won the game!</h1>
 }
   
